c3976baaac7719300a005e6a0113c8d27322439a
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-03-28 19:23:32 +0300

    add project list and get endpoints

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.schemas import ProjectCreate, ProjectRead
@@ -12,7 +14,6 @@ router = APIRouter()
 @router.post("/projects/", response_model=ProjectRead)
 def create_project(payload: ProjectCreate, db: Session = Depends(get_db())):
 
-
     new_project = Project(
         owner_id=1, #временно, пока нет get_current_user
         name=payload.name,
@@ -23,3 +24,16 @@ def create_project(payload: ProjectCreate, db: Session = Depends(get_db())):
     db.refresh(new_project)
 
     return new_project
+
+@router.get("/projects/", response_model=List[ProjectRead])
+def list_projects(db: Session = Depends(get_db)):
+    projects = db.query(Project).all()
+    return projects
+
+@router.get("/projects/{project_id}", response_model=ProjectRead)
+def project(project_id: int, db: Session = Depends(get_db)):
+    project = db.query(Project).filter(Project.id == project_id).first()
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    return project
