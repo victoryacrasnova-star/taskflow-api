11f2981d44b0205f5864116d76ba7e290fa58a10
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-04-06 21:13:02 +0300

    move DATABASE_URL to environment variables

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/taskflow-db"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
     autocommit=False,
