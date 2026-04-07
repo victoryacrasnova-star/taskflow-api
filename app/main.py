3063306793851580f4d7fc23dc291b31c4e0aa63
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-04-07 20:58:13 +0300

    update: in main add router project and task

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
-
-from app.routers import auth
+from app.routers import auth, projects, task
 
 app = FastAPI()
-app.include_router(auth.router)
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(auth.router)
+app.include_router(projects.router)
+app.include_router(task.router)
